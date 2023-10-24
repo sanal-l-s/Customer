716acfb084ec9e978500a97c9db4b1787c5ad313
Sanal L S <sanallsofficial@gmail.com>
2023-10-24 10:26:10 +0530

    home page designed

--- app/src/main/java/com/example/customer/MainActivity.kt
+++ app/src/main/java/com/example/customer/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.customer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customer.modules.homepage.HomePageActivity
+import com.example.customer.modules.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startHomeActivity()
+    }
+
+    private fun startLoginActivity() {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    private fun startHomeActivity() {
+        startActivity(Intent(this, HomePageActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/example/customer/modules/homepage/HomePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.customer.modules.homepage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customer.R
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
